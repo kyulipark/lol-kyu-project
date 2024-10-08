47fa67f41f9f36dc953570b56d5a7b5a8f58deb5
t2023-m0106 <t2023-m0106@t2023-m0106ui-MacBookAir.local>
2024-10-08 12:27:18 +0900

    add: 챔피언 카드 구현

--- src/app/champions/_components/ChampionCard.tsx
+++ src/app/champions/_components/ChampionCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-  champiion: Champion;
+  champion: Champion;
 }
 
 export default function ChampionCard({ champion }: Props) {
